4c05908c52b8e5c61451c240766dc9bee4f6eac1
nozwock <57829219+nozwock@users.noreply.github.com>
2025-03-02 15:04:01 +0530

    Multiply fishing experience in the generated pak

--- MoreGatherRewards/src/main.py
+++ MoreGatherRewards/src/main.py
@@ -2,11 +2,10 @@ import json
 import platform
 import shutil
 import subprocess
-import sys
 import tempfile
 from argparse import ArgumentParser
-from functools import reduce
 from pathlib import Path
+from typing import Any
 
 
 def is_file(path: Path, parser: ArgumentParser):
@@ -14,7 +13,61 @@ def is_file(path: Path, parser: ArgumentParser):
         parser.error(f"File not found '{path}'")
 
 
-def patch_asset_json(asset_table: dict):
+def json_loads(fromfile: Path) -> Any:
+    with open(
+        fromfile,
+        encoding="utf-8-sig",
+    ) as f:
+        return json.loads(f.read())
+
+
+def json_dumps(obj: Any, tofile: Path):
+    with open(tofile, "wb") as f:
+        f.write(json.dumps(obj, ensure_ascii=False, indent=2).encode())
+
+
+def convert_uasset_json(
+    uassetgui: Path,
+    tojson: bool,
+    input_paths: list[Path],
+    output_paths: list[Path],
+    version: tuple[int, int] = (4, 26),
+):
+    def convert(input_path: Path, output_path: Path):
+        args = [
+            uassetgui,
+            "tojson" if tojson else "fromjson",
+            input_path,
+            output_path,
+            f"VER_UE{version[0]}_{version[1]}",
+        ]
+
+        match platform.system():
+            case "Linux":
+                args[2:4] = [
+                    it.decode()
+                    for it in subprocess.run(
+                        ["winepath", "-0", "-w", *args[2:4]],
+                        capture_output=True,
+                        check=True,
+                    ).stdout.split(b"\0")
+                    if it.decode().strip()
+                ]
+
+                # note: Assuming the only public build of UAssetGUI is a Window PE
+                subprocess.run(["wine"] + args, check=True, capture_output=True)
+            case "Windows":
+                subprocess.run(args, check=True, capture_output=True)
+            case _:
+                raise Exception("Unsupported system")
+
+    assert len(input_paths) == len(output_paths)
+
+    for input_path, output_path in zip(input_paths, output_paths):
+        convert(input_path, output_path)
+
+
+def patch_gathers_asset_json(asset_table: dict, mfactor: float):
     assert asset_table["$type"].find("UAssetAPI") != -1
 
     data_table = asset_table["Exports"][0]["Table"]["Data"]
@@ -43,55 +96,30 @@ def patch_asset_json(asset_table: dict):
                         lambda it: it.get("StructType") == "ActionSetting",
                         gather_action_setting["Value"],
                     ):
-                        num_obj = reduce(
-                            lambda _, it: it if it["Name"] == "Num" else None,
-                            action_setting["Value"],
+                        num_obj = next(
+                            filter(
+                                lambda it: it["Name"] == "Num",
+                                action_setting["Value"],
+                            ),
                             None,
                         )
                         if num_obj and num_obj["Value"] > 0:
-                            num_obj["Value"] = num_obj["Value"] * args.factor
+                            num_obj["Value"] = num_obj["Value"] * mfactor
 
                 break
 
 
-def convert_uasset_json(
-    uassetgui: Path,
-    tojson: bool,
-    input_path: Path,
-    output_path: Path,
-    version: tuple[int, int] = (4, 26),
-):
-    args = [
-        uassetgui,
-        "tojson" if tojson else "fromjson",
-        input_path,
-        output_path,
-        f"VER_UE{version[0]}_{version[1]}",
-    ]
+def patch_fishing_asset_json(asset_table: dict, mfactor: int):
+    assert asset_table["$type"].find("UAssetAPI") != -1
 
-    match platform.system():
-        case "Linux":
-            args[2:4] = [
-                it.decode()
-                for it in subprocess.run(
-                    ["winepath", "-0", "-w", *args[2:4]],
-                    capture_output=True,
-                    check=True,
-                ).stdout.split(b"\0")
-                if it.decode().strip()
-            ]
+    data_table = asset_table["Exports"][0]["Table"]["Data"]
 
-            subprocess.run(
-                ["wine"] + args,
-                check=True,
-            )
-        case "Windows":
-            subprocess.run(
-                args,
-                check=True,
-            )
-        case _:
-            raise Exception("Unsupported system")
+    for fishing_item in data_table:
+        exp_obj = next(
+            filter(lambda it: it["Name"] == "Exp", fishing_item["Value"]), None
+        )
+        if exp_obj and exp_obj["Value"] > 0:
+            exp_obj["Value"] = exp_obj["Value"] * mfactor
 
 
 if __name__ == "__main__":
@@ -104,10 +132,10 @@ if __name__ == "__main__":
     parser.add_argument(
         "-i",
         "--input",
-        metavar="FILE",
         required=True,
+        metavar="FILE",
         type=Path,
-        help="Path to Game's '.pak' file or 'Gathers.json' exported from it via UAssetGUI",
+        help="Path to the Game's '.pak' file",
     )
     parser.add_argument(
         "-o",
@@ -117,118 +145,114 @@ if __name__ == "__main__":
     )
     parser.add_argument("-f", "--force", action="store_true")
     parser.add_argument(
-        "--factor",
+        "--gather-factor",
         metavar="FLOAT",
         default=3,
         type=float,
         help="[default: 3] Multiplicative factor for resource and experience gain on item gathering",
     )
     parser.add_argument(
+        "--fishing-factor",
+        metavar="INT",
+        default=5,
+        type=int,
+        help="[default: 5] Multiplicative factor experience gain on fishing",
+    )
+    parser.add_argument(
         "--uassetgui",
+        required=True,
         metavar="PATH",
         type=Path,
         help="Path to UAssetGUI (https://github.com/atenfyr/UAssetGUI)",
     )
     parser.add_argument(
         "--repak",
+        required=True,
         metavar="PATH",
         type=Path,
         help="Path to trumank/repak (https://github.com/trumank/repak)",
     )
 
     args = parser.parse_args()
-    is_file(args.input, parser)
+
+    for it in (args.input, args.uassetgui, args.repak):
+        is_file(it, parser)
+
+    args.fishing_factor = max(args.fishing_factor, 1)
+    args.gather_factor = max(args.gather_factor, 1)
 
     output_path = args.output or Path(f"{args.input.stem}.Patched{args.input.suffix}")
 
     if output_path.exists() and not args.force:
         parser.error(f"File already exists '{output_path}'")
 
-    match args.input.suffix.lower().strip("."):
-        case "json":
-            gathers: dict
-            with open(
+    if args.input.suffix.lower() != ".pak":
+        parser.error("Only '.pak' files are allowed as input")
+
+    tables_dir = "Wandering_Sword/Content/JH/Tables"
+    with tempfile.TemporaryDirectory() as workdir:
+        workdir = Path(workdir)
+        subprocess.run(
+            [
+                args.repak,
+                "unpack",
+                "-f",
+                "-i",
+                f"{tables_dir}/Gathers.*",
+                "-i",
+                f"{tables_dir}/Fishing.*",
+                "-o",
+                workdir,
                 args.input,
-                encoding="utf-8-sig",
-            ) as f:
-                gathers = json.loads(f.read())
+            ],
+            check=True,
+            capture_output=True,
+        )
 
-            patch_asset_json(gathers)
+        tables_workdir = workdir.joinpath(tables_dir)
 
-            with open(output_path, "wb") as f:
-                f.write(json.dumps(gathers, ensure_ascii=False, indent=2).encode())
-        case "pak":
-            pak_required_msg = "required to process '.pak' files:"
-            if not args.uassetgui:
-                parser.error(f"{pak_required_msg} --uassetgui")
-            is_file(args.uassetgui, parser)
+        gathers_uasset_path = tables_workdir.joinpath("Gathers.uasset")
+        gathers_json_path = workdir.joinpath("Gathers.json")
 
-            if not args.repak:
-                parser.error(f"{pak_required_msg} --repak")
-            is_file(args.repak, parser)
+        fishing_uasset_path = tables_workdir.joinpath("Fishing.uasset")
+        fishing_json_path = workdir.joinpath("Fishing.json")
 
-            gathers_dir = "Wandering_Sword/Content/JH/Tables"
-            with tempfile.TemporaryDirectory() as workdir:
-                workdir = Path(workdir)
-                subprocess.run(
-                    [
-                        args.repak,
-                        "unpack",
-                        "-f",
-                        "-i",
-                        f"{gathers_dir}/Gathers.*",
-                        "-o",
-                        workdir,
-                        args.input,
-                    ],
-                    check=True,
-                )
+        convert_uasset_json(
+            args.uassetgui,
+            tojson=True,
+            input_paths=[gathers_uasset_path, fishing_uasset_path],
+            output_paths=[gathers_json_path, fishing_json_path],
+        )
 
-                gathers_uasset_path = workdir.joinpath(gathers_dir).joinpath(
-                    "Gathers.uasset"
-                )
-                gathers_json_path = workdir.joinpath("Gathers.json")
+        gathers_asset = json_loads(gathers_json_path)
+        patch_gathers_asset_json(gathers_asset, args.gather_factor)
+        json_dumps(gathers_asset, gathers_json_path)
 
-                convert_uasset_json(
-                    args.uassetgui,
-                    tojson=True,
-                    input_path=gathers_uasset_path,
-                    output_path=gathers_json_path,
-                )
+        fishing_asset = json_loads(fishing_json_path)
+        patch_fishing_asset_json(fishing_asset, args.fishing_factor)
+        json_dumps(fishing_asset, fishing_json_path)
 
-                gather: dict
-                with open(
-                    gathers_json_path,
-                    encoding="utf-8-sig",
-                ) as f:
-                    gathers = json.loads(f.read())
+        convert_uasset_json(
+            args.uassetgui,
+            tojson=False,
+            input_paths=[gathers_json_path, fishing_json_path],
+            output_paths=[gathers_uasset_path, fishing_uasset_path],
+        )
 
-                patch_asset_json(gathers)
+        for it in (gathers_json_path, fishing_json_path):
+            it.unlink(True)
 
-                with open(gathers_json_path, "wb") as f:
-                    f.write(json.dumps(gathers, ensure_ascii=False, indent=2).encode())
+        # note: Not using compression since that seem to break the pak generated by repak
+        subprocess.run(
+            [
+                args.repak,
+                "pack",
+                workdir,
+            ],
+            check=True,
+            capture_output=True,
+        )
 
-                convert_uasset_json(
-                    args.uassetgui,
-                    tojson=False,
-                    input_path=gathers_json_path,
-                    output_path=gathers_uasset_path,
-                )
+        shutil.move(workdir.parent.joinpath(f"{workdir.name}.pak"), output_path)
 
-                gathers_json_path.unlink(True)
-
-                # note: Not using compression since that seem to break the pak generated by repak
-                subprocess.run(
-                    [
-                        args.repak,
-                        "pack",
-                        workdir,
-                    ],
-                    check=True,
-                )
-
-                shutil.move(workdir.parent.joinpath(f"{workdir.name}.pak"), output_path)
-        case _:
-            parser.error(f"Can't handle file of type '{args.input.suffix}'")
-
-    print(f"Saved to '{output_path}'", file=sys.stderr)
+    print(f"Saved to '{output_path}'")
