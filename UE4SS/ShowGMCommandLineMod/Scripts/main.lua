0740e706eb22c3794d6b7e29abd9014088fb85cd
nozwock <57829219+nozwock@users.noreply.github.com>
2025-03-08 12:44:35 +0530

    Use default instance of JHNeoUISubsystem in ShowGMCommandLine
    And, refactor the mod using RegisterMod API.

--- UE4SS/ShowGMCommandLineMod/Scripts/main.lua
+++ UE4SS/ShowGMCommandLineMod/Scripts/main.lua
@@ -1,7 +1,39 @@
-RegisterKeyBind(Key.OEM_THREE --[[Tidle]], function ()
-    local JHNeoUISubsystem = FindFirstOf("JHNeoUISubsystem")
-    ExecuteInGameThread(function ()
-        JHNeoUISubsystem:ShowGMCommandLine()
-    end)
-end)
+local UEHelpers = require("UEHelpers")
 
+---@param msg string
+local function Log(msg)
+	print("[ShowGMCommandLine] " .. msg)
+end
+
+--- Runs callback once PlayerController is available
+---@param InitCallback function
+local function RegisterMod(InitCallback)
+	if pcall(UEHelpers.GetPlayerController) then
+		-- For hot-reloading
+		InitCallback()
+	else
+		local InitHookIds
+		local PreId, PostId = RegisterHook("/Script/Engine.PlayerController:ClientRestart", function()
+			if InitHookIds then
+				UnregisterHook("/Script/Engine.PlayerController:ClientRestart", table.unpack(InitHookIds))
+			else
+				Log("Failed to unregister Init hook")
+			end
+
+			InitCallback()
+		end)
+
+		InitHookIds = { PreId, PostId }
+	end
+end
+
+RegisterMod(function()
+	local JHNeoUISubsystem = StaticFindObject("/Script/JH.Default__JHNeoUISubsystem")
+	assert(JHNeoUISubsystem:IsValid())
+
+	RegisterKeyBind(Key.OEM_THREE --[[Tidle]], function()
+		ExecuteInGameThread(function()
+			JHNeoUISubsystem:ShowGMCommandLine()
+		end)
+	end)
+end)
